fe4438c50ee0fea5fe1739d986fddff760f9e680
ETHEVE Evan <evan.etheve@GB42.local>
2025-03-24 17:33:02 +0200

    [UPDATE ET ADD] ajout d'une documentation tech et modif joueur

--- Joueur.py
+++ Joueur.py
@@ -1,38 +1,40 @@
 import random
-from Terrain import Terrain 
+from terrain import Terrain 
 
 class Joueur:
 
-    def __init__(self, nom, terrain, solde = 2000 , position = 0 ):
+    def __init__(self, nom, solde = 2000 , position = 0 ):
         self.nom = nom
         self.solde = solde 
         self.position = position 
-        self.terter = terrain
+        self.terter = []
 
 
     def tirer_de(self):
         tirer = random.randint(1,6)
-        print (f"Le joueur {self.joueur} a tiré ")
+        print (f"Le joueur {self.joueur} a tiré " , tirer , )
+        #self.position += tirer
+        return tirer
 
     def deplacement(self, nb_cases):
         
-        self.position += nb_cases
+        self.position += nb_cases  #tirer
 
     def acheter(self, terrain):
         """
             Le joueur achete le terrain terrain
         """
         #possede = []
-        achete = input(f'Voulez vous acheter le terrain {self.terter.getNom()} ? (0/1)')
+        achete = input(f'Voulez vous acheter le terrain {terrain.getNom()} ? (0/1)')
         if achete == 1 :
-            if self.solde >= self.terter.getPrix() :
-                self.solde -= self.terter.getPrix()
-                print (f"Le joueur {self.nom} a acheté le terrain {self.terter.getNom()} pour le prix de {self.terter.getPrix()} € , il lui reste donc {self.solde} €")
-                #possede.append(self.terter.getNom)
+            if self.solde >= terrain.getPrix() :
+                self.solde -= terrain.getPrix()
+                print (f"Le joueur {self.nom} a acheté le terrain {terrain.getNom()} pour le prix de {terrain.getPrix()} € , il lui reste donc {self.solde} €")
+                self.terter.append(terrain)
             else:
-                print (f"Le joueur {self.nom} n'a pas assez d'argent pour acheter le terrain {self.terter.getNom()} , car le prix était de {self.terter.getPrix()}, et il n'avait que {self.solde} €")
+                print (f"Le joueur {self.nom} n'a pas assez d'argent pour acheter le terrain {terrain.getNom()} , car le prix était de {terrain.getPrix()}, et il n'avait que {self.solde} €")
         else: 
-            print(f"Le joueur{self.nom} ne veut pas acheter le terrain {self.terter.getNom()} puis passe son tour ")
+            print(f"Le joueur{self.nom} ne veut pas acheter le terrain {terrain.getNom()} puis passe son tour ")
         
 
     def payer(self,terrain, autre_joueur):
